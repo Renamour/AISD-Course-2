6e192db8e9b3b9570c6059b422794bbf57bdb22c
Renamour <amina.qew@mail.ru>
2022-11-07 13:53:59 +0400

    Lab Work 1

---
+++ Lab Work 1/Lending/app/views.py
@@ -0,0 +1,16 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/landing.html',
+        
+    )
---
+++ Lab Work 1/Lending/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\avton\source\repos\Lending\Lending\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
